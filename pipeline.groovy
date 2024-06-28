fe822e38e20a2912cd8afff9414649841136d1f8
Nadun Dissanayakke <139664686+Nadun-Dissanayake@users.noreply.github.com>
2024-06-29 00:01:14 +0530

    Update pipeline.groovy

--- pipeline.groovy
+++ pipeline.groovy
@@ -5,6 +5,8 @@ pipeline {
         REPO_URL = 'https://github.com/Nadun-Dissanayake/Art-and-Craft-Fullstack.git'
         BRANCH = 'main'
         APP_NAME = 'Art and Craft Ecommerce Website'
+        FRONTEND_IMAGE = 'ndissanayake/devops_frontend:latest'
+        BACKEND_IMAGE = 'ndissanayake/devops_backend:latest'
     }
 
     stages {
@@ -34,23 +36,71 @@ pipeline {
             }
         }
 
-        stage('Login to Docker Hub') {
+        stage('Tag Docker Images') {
             steps {
-                withCredentials([string(credentialsId: 'duckerhubpassword', variable: 'mernapp')]) {
-                    script {
-                        bat "docker login -u ndissanayake -p ${mernapp}"
+                script {
+                    if (isUnix()) {
+                        sh """
+                        docker tag devops_frontend:latest ${FRONTEND_IMAGE}
+                        docker tag devops_backend:latest ${BACKEND_IMAGE}
+                        """
+                    } else {
+                        bat """
+                        docker tag devops_frontend:latest ${FRONTEND_IMAGE}
+                        docker tag devops_backend:latest ${BACKEND_IMAGE}
+                        """
                     }
                 }
             }
         }
-        stage('Push Image Frontend') {
+
+        stage('Verify Docker Images') {
             steps {
-                bat 'docker push ndissanayake/devops_frontend:latest'
+                script {
+                    if (isUnix()) {
+                        sh 'docker images'
+                    } else {
+                        bat 'docker images'
+                    }
+                }
             }
         }
+
+        stage('Login to Docker Hub') {
+            steps {
+                withCredentials([string(credentialsId: 'duckerhubpassword', variable: 'DOCKER_HUB_PASSWORD')]) {
+                    script {
+                        if (isUnix()) {
+                            sh 'echo $DOCKER_HUB_PASSWORD | docker login -u ndissanayake --password-stdin'
+                        } else {
+                            bat 'docker login -u ndissanayake -p %DOCKER_HUB_PASSWORD%'
+                        }
+                    }
+                }
+            }
+        }
+
+        stage('Push Image Frontend') {
+            steps {
+                script {
+                    if (isUnix()) {
+                        sh "docker push ${FRONTEND_IMAGE}"
+                    } else {
+                        bat "docker push ${FRONTEND_IMAGE}"
+                    }
+                }
+            }
+        }
+
         stage('Push Image Backend') {
             steps {
-                bat 'docker push ndissanayake/devops_backend:latest'
+                script {
+                    if (isUnix()) {
+                        sh "docker push ${BACKEND_IMAGE}"
+                    } else {
+                        bat "docker push ${BACKEND_IMAGE}"
+                    }
+                }
             }
         }
 
