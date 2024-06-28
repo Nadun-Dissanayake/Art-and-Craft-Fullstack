10a1d99dd663fe8fa2446d7f48357d6f26b46f52
Nadun Dissanayakke <139664686+Nadun-Dissanayake@users.noreply.github.com>
2024-06-28 22:06:44 +0530

    Update pipeline.groovy

--- pipeline.groovy
+++ pipeline.groovy
@@ -1,17 +1,13 @@
 pipeline {
     agent any
 
-    triggers {
-        githubPush()
-    }
-
     environment {
         REPO_URL = 'https://github.com/Nadun-Dissanayake/Art-and-Craft-Fullstack.git'
         BRANCH = 'main'
         APP_NAME = 'Art and Craft Ecommerce Website'
     }
 
-  stages {
+    stages {
         stage('Clone Repository') {
             steps {
                 git branch: "${BRANCH}", url: "${REPO_URL}"
@@ -21,7 +17,11 @@ pipeline {
         stage('Build Docker Images') {
             steps {
                 script {
-                    sh 'docker-compose build'
+                    if (isUnix()) {
+                        sh 'docker-compose build'
+                    } else {
+                        bat 'docker-compose build'
+                    }
                 }
             }
         }
@@ -29,9 +29,11 @@ pipeline {
         stage('Push Docker Images') {
             steps {
                 script {
-                    sh '''
-                    docker-compose push
-                    '''
+                    if (isUnix()) {
+                        sh 'docker-compose push'
+                    } else {
+                        bat 'docker-compose push'
+                    }
                 }
             }
         }
@@ -39,12 +41,32 @@ pipeline {
         stage('Deploy Application') {
             steps {
                 script {
-                    sh 'docker-compose down'
-                    sh 'docker-compose up -d'
+                    if (isUnix()) {
+                        sh '''
+                        docker-compose down
+                        docker-compose up -d
+                        '''
+                    } else {
+                        bat '''
+                        docker-compose down
+                        docker-compose up -d
+                        '''
+                    }
                 }
             }
         }
-    }  
+    }
 
-  
+    post {
+        always {
+            script {
+                echo 'Cleaning up...'
+                if (isUnix()) {
+                    sh 'docker-compose down'
+                } else {
+                    bat 'docker-compose down'
+                }
+            }
+        }
+    }
 }
