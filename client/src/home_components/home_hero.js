c01843218b6b37044468f76f0d767eadf5464c9f
Nadun Dissanayakke <139664686+Nadun-Dissanayake@users.noreply.github.com>
2025-04-03 08:22:00 +0530

    Update home_hero.js

--- client/src/home_components/home_hero.js
+++ client/src/home_components/home_hero.js
@@ -34,7 +34,7 @@ export default function Home_hero(){
             <div className="home_hero">
                 <div className="discription">
                     <h1>Find the modern creative <br/> Art and Craft Products in Sri Lanka</h1>
-                    <button>Join Now?</button>
+                    <button>Join Now??</button>
                 </div>
                 <div className="home_img" >
                     <img src={b1}/>
